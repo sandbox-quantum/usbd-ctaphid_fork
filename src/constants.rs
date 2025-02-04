0174b08b2dc1740006135c7e07afd4c50aa04e9d
Kyle Kotowick <kotowick@invictonlabs.com>
2025-02-04 14:48:54 -0500

    Update message size

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,18 @@ pub const INTERRUPT_POLL_MILLISECONDS: u8 = 5;
 
 pub const PACKET_SIZE: usize = 64;
 
+<<<<<<< Updated upstream
 // 7609
 pub const MESSAGE_SIZE: usize = PACKET_SIZE - 7 + 128 * (PACKET_SIZE - 5);
+=======
+// TODO: find the actual minimum Dilithium sizes
+pub const MESSAGE_SIZE: usize = (if cfg!(feature = "backend-dilithium5") {
+    20000
+} else if cfg!(feature = "backend-dilithium3") {
+    15000
+} else if cfg!(feature = "backend-dilithium2") {
+    10000
+} else {
+    3072
+});
+>>>>>>> Stashed changes
